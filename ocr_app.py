08f263f94c7852af7e05af8b906b5b3200e7de24
AMRUTHA <63155923+AMRUTHA2002@users.noreply.github.com>
2023-09-25 03:53:13 +0530

    Add files via upload

--- ocr_app.py
+++ ocr_app.py
@@ -2,46 +2,75 @@ import easyocr as ocr
 import streamlit as st
 from PIL import Image
 import numpy as np
+import streamlit_authenticator as stauth
+import pickle
+from pathlib import Path
 
-#title
-st.title("Blueprint OCR")
+st.set_page_config(page_title="Blueprint OCR", page_icon=":🔎:", layout="wide")
 
-#subtitle
-st.markdown("## Built using `easyocr`")
+# --- USER AUTHENTICATION ---
+names = ["Amrutha", "Ansika"]
+usernames = ["amruthuse", "ansika123"]
 
-st.markdown("")
+# load hashed passwords
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
 
-#image uploader
-image = st.file_uploader(label = "Upload your image here",type=['png','jpg','jpeg'])
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+    "userCookie", "abcdef", cookie_expiry_days=30)
 
+name, authentication_status, username = authenticator.login("Login", "main")
 
-@st.cache
-def load_model(): 
-    reader = ocr.Reader(['en'],model_storage_directory='.',gpu=True)
-    return reader 
+if authentication_status == False:
+    st.error("Username/password is incorrect")
 
-reader = load_model() #load model
-
-if image is not None:
-
-    input_image = Image.open(image) #read image
-    st.image(input_image) #display image
-
-    with st.spinner("🤖 Extracting... "):
-        
-
-        result = reader.readtext(np.array(input_image))
-
-        result_text = [] #empty list for results
+if authentication_status == None:
+    st.warning("Please enter your username and password")
 
 
-        for text in result:
-            result_text.append(text[1])
 
-        st.write(result_text)
-    #st.success("Here you go!")
-    st.balloons()
-else:
-    st.write("Upload an Image")
+if authentication_status:
 
-st.caption("Here is the text extracted from your image...")
+    authenticator.logout("Logout", "sidebar")
+    st.sidebar.title(f"Welcome {name}")
+
+    st.title("Blueprint OCR")
+
+    st.markdown("## Built using `easyocr`")
+
+    st.markdown("")
+
+    image = st.file_uploader(label = "Upload your image here",type=['png','jpg','jpeg'])
+
+
+    @st.cache_resource
+    def load_model(): 
+        reader = ocr.Reader(['en'],model_storage_directory='.',gpu=True)
+        return reader 
+
+    reader = load_model()
+
+    if image is not None:
+
+        input_image = Image.open(image) 
+        st.image(input_image) 
+
+        with st.spinner("🤖 Extracting... "):
+            
+
+            result = reader.readtext(np.array(input_image))
+
+            result_text = [] 
+
+
+            for text in result:
+                result_text.append(text[1])
+
+            st.write(result_text)
+        st.balloons()
+    else:
+        st.write("Upload an Image")
+
+    st.caption("Here is the text extracted from your image...")
+
